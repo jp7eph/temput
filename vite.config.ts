06717552f375c11195b8e2640799200a6dd665f7
jp7eph <jp7eph@gmail.com>
2023-03-29 21:26:38 +0900

    wip

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, version } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
+// import manifest from './manifest.json' // Node 14 & 16
+// import manifest from './manifest.json' assert { type: 'json' } // Node >=17
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'temput',
+  description: 'Enter template text',
+  version: '0.0.1',
+  icons: {
+    '16': 'icons/temput_16.png',
+    '32': 'icons/temput_32.png',
+    '48': 'icons/temput_48.png',
+    '128': 'icons/temput_128.png',
+  },
+  action: {
+    'default_popup': 'index.html'
+  },
+})
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    crx({ manifest }),
+  ],
+})
