fe5a54514cadc3bfeab964a7cf56de31cc05a084
Naoki Horie <jp7eph@gmail.com>
2023-04-18 01:45:14 +0900

    クリップボードへ書き込み (#8)

---
+++ src/components/temput.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+function paste() {
+  console.log("clicked!");
+  const text = "hogehoge";
+  const type = "text/plain";
+  const blob = new Blob([text], { type });
+  const data = [new ClipboardItem({ [type]: blob })];
+  navigator.clipboard.write(data);
+}
+</script>
+
+<template>
+  <button type="button" @click="paste">copy!</button>
+</template>
+
+<style scoped></style>
