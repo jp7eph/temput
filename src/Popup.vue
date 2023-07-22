950a5ddeb73d5e214cda7bf705d034b4a1f344db
jp7eph <jp7eph@gmail.com>
2023-07-23 00:13:22 +0900

    wip

--- src/Popup.vue
+++ src/Popup.vue
@@ -1,16 +1,68 @@
 <script setup lang="ts">
-function paste() {
-  console.log("clicked!");
-  const text = "<ul><li><p>aaa</p><ul><li><p><strong>bbb</strong></p></li></ul></li></ul>";
+import { ref } from "vue";
+import { Template } from "./template";
+
+const templates: Template[] = [];
+const templatesRef = ref(templates);
+
+// TODO: オプションのロードと共通化したい
+function loadTemplate() {
+  chrome.storage.local.get("templates", function (value) {
+    // ストレージに保存するとObjectになってしまうのでArrayに変換
+    const t = Object.values(value.templates);
+    if (Array.isArray(t)) {
+      templatesRef.value = [...(t as Template[])];
+      console.debug("template loaded!");
+    } else {
+      console.error("err: saved value tranlate to array!");
+    }
+  });
+}
+loadTemplate();
+
+function openConfig() {
+  chrome.runtime.openOptionsPage();
+}
+
+function writeClipBoard(value: string) {
+  const text = value;
   const type = "text/html";
   const blob = new Blob([text], { type });
   const data = [new ClipboardItem({ [type]: blob })];
   navigator.clipboard.write(data);
+  console.debug("write clipboard: " + value);
 }
 </script>
 
 <template>
-  <button type="button" @click="paste">copy!</button>
+  <v-layout class="rounded rounded-md" style="min-width: 300px">
+    <v-app-bar title="テンプレート一覧" class="bg-blue">
+      <v-btn color="white" icon="$cog" @click="openConfig"></v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-list lines="one">
+        <v-list-item v-for="template in templatesRef" :key="template.id">
+          <template v-slot="">
+            <v-row no-gutters>
+              <v-col cols="4" class="d-flex justify-start text-disabled">
+                #{{ template.id.split("-")[0] }}
+              </v-col>
+              <v-col cols="8">
+                <span> {{ template.name }} </span>
+              </v-col>
+            </v-row>
+          </template>
+          <template v-slot:append>
+            <v-btn
+              icon="$paste"
+              variant="text"
+              @click="writeClipBoard(template.value)"
+            ></v-btn>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-main>
+  </v-layout>
 </template>
 
 <style scoped></style>
